e683aa7fd22523df60cf3364efb2b1170bf66634
Liam Frye-Mason <lfryemason>
2019-01-17 11:24:49 -0700

    Added main app stub. Added basic firebase framework to project. Requires a .env file in the main directory that contains config data.

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,19 @@
+import app from 'firebase/app';
+const config = {
+    apiKey: REACT_APP_API_KEY,
+    authDomain: REACT_APP_AUTH_DOMAIN,
+    databaseURL: REACT_APP_DATABASE_URL,
+    projectId: REACT_APP_PROJECT_ID,
+    storageBucket: REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_MESSAGING_SENDER_ID
+}
+
+class Firebase
+{
+    constructor()
+    {
+        app.initializeApp(config);
+    }
+}
+
+export default Firebase;
