25defb91f9b6d04e63c4ca96608305820734ce40
Liam Frye-Mason <lfryemason>
2019-01-20 15:41:22 -0700

    Added routing and navigation component. Added a basic landing page to test the navigation and prevent errors.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router,
+         Route,
+        } from 'react-router-dom';
+
+import Navigation from '../Navigation';
+import LandingPage from '../Landing';
+
+import * as ROUTES from '../../constants/routes';
 
 const App = () => (
-    <div>
-        <h1>App</h1>
-    </div>
+    <Router>
+        <div>
+            <Navigation />
+
+            <Route exact path={ROUTES.LANDING} component={LandingPage} />
+        </div>
+    </Router>
 );
 
 export default App;
