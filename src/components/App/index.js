60e6d3485407856bb55047820b5ecdf34651dbd1
Liam Frye-Mason <lfryemason>
2019-01-21 11:51:10 -0700

    Added sign out button and authentication framework. Sign in, sign out and sign up work, the navigation component displays information specific to authentication status

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router,
          Route,
         } from 'react-router-dom';
+import { withFirebase } from '../Firebase';
 
 import Navigation from '../Navigation';
 import LandingPage from '../Landing';
@@ -10,16 +11,44 @@ import SignInPage from '../SignIn';
 
 import * as ROUTES from '../../constants/routes';
 
-const App = () => (
-    <Router>
-        <div>
-            <Navigation />
+class App extends Component
+{
+    constructor(props)
+    {
+        super(props);
 
-            <Route exact path={ROUTES.LANDING} component={LandingPage} />
-            <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-            <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-        </div>
-    </Router>
-);
+        this.state = { authUser: null };    
+    }
 
-export default App;
+
+    componentDidMount()
+    {
+        this.listener = this.props.firebase.auth.onAuthStateChanged(authUser => {
+            authUser
+                ? this.setState({ authUser })
+                : this.setState({ authUser: null });
+        });
+    }
+
+    componentWillUnmount()
+    {
+        this.listener();
+    }
+
+    render()
+    {
+        return(
+            <Router>
+                <div>
+                    <Navigation authUser={this.state.authUser} />
+
+                    <Route exact path={ROUTES.LANDING} component={LandingPage} />
+                    <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+                    <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+                </div>
+            </Router>
+        );
+    }
+}
+
+export default withFirebase(App);
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -3,23 +3,28 @@ import { Link } from 'react-router-dom';
 
 import * as ROUTES from '../../constants/routes';
 
-const Navigation = () =>
-    <div> 
+import SignOutButton from '../SignOut';
+import { SignInForm } from '../SignIn';
+
+const Navigation = ({ authUser} ) => (
+        <div>{authUser ? <NavigationAuth /> : <br />}</div>
+);
+
+const NavigationAuth = () => (
+    <div>
         <h2>Navigation</h2>
         <ul>
-            <li>
-                <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-            </li>
-            <li>
-                <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
-            </li>
             <li>
                 <Link to={ROUTES.ACCOUNT}>Account</Link>
             </li>
             <li>
                 <Link to={ROUTES.HOME}>Home</Link>
             </li>
+            <li>
+                <SignOutButton />
+            </li>
         </ul>
     </div>
+);
 
 export default Navigation;
