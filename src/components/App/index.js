c69700c48a49e8b8585600bf40ea91ff03e9d238
Liam Frye-Mason <lfryemason>
2019-01-27 12:42:47 -0700

    Added a higher component to take care of all authentication and authenticated user updates. This allows all leaf components to know about the authUser without passing through all levels.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,54 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router,
          Route,
-        } from 'react-router-dom';
-import { withFirebase } from '../Firebase';
+         } from 'react-router-dom';
 
 import Navigation from '../Navigation';
 import LandingPage from '../Landing';
 import SignUpPage from '../SignUp';
 import SignInPage from '../SignIn';
+import { withAuthentication } from '../Session';
 
 import * as ROUTES from '../../constants/routes';
 
-class App extends Component
-{
-    constructor(props)
-    {
-        super(props);
+const App = () => (
+  <Router>
+    <div>
+      <Navigation />
 
-        this.state = { authUser: null };    
-    }
+      <hr />
 
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+    </div>
+  </Router>
+);
 
-    componentDidMount()
-    {
-        this.listener = this.props.firebase.auth.onAuthStateChanged(authUser => {
-            authUser
-                ? this.setState({ authUser })
-                : this.setState({ authUser: null });
-        });
-    }
-
-    componentWillUnmount()
-    {
-        this.listener();
-    }
-
-    render()
-    {
-        return(
-            <Router>
-                <div>
-                    <Navigation authUser={this.state.authUser} />
-
-                    <Route exact path={ROUTES.LANDING} component={LandingPage} />
-                    <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-                    <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-                </div>
-            </Router>
-        );
-    }
-}
-
-export default withFirebase(App);
+export default withAuthentication(App);
