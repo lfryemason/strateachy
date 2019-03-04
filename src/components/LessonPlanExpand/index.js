1744eab6b0bf3aa548a5de33983f3b6651553c34
Liam Frye-Mason <lfryemason>
2019-03-04 15:13:31 -0700

    Implemented deleting lesson plans

--- src/components/LessonPlanExpand/index.js
+++ src/components/LessonPlanExpand/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import { withStore } from '../../Store';
 
+import DeleteModal from '../DeleteModal';
+
 import { withFirestore } from 'react-firestore';
 
 import DatePicker from "react-datepicker";
@@ -13,6 +15,14 @@ import "./index.css"
 
 class LessonPlanExpand extends Component
 {
+  constructor(props)
+  {
+    super(props);
+
+    this.state = {
+      isDeleteModalOpen: false,
+    };
+  }
   //Save the lesson plan to the database.
   onSubmit = event =>
   {
@@ -38,6 +48,20 @@ class LessonPlanExpand extends Component
     this.props.store.updateCurrentLessonPlan({ [event.target.name]: event.target.value });
   };
 
+  deleteEvent = event =>
+  {
+    this.props.firestore.collection("lessonPlans").doc(this.props.store.currentLessonPlanID).delete();
+    this.props.store.newCurrentLessonPlan();
+    this.toggleDeleteModalOpen();
+  }
+
+  toggleDeleteModalOpen = () => 
+  {
+    console.log("open modal")
+    const { isDeleteModalOpen } = this.state; 
+    this.setState({isDeleteModalOpen: ! isDeleteModalOpen});
+  }
+
   dateChange = date =>
   {
     this.props.store.updateCurrentLessonPlan({ date: date });
@@ -46,82 +70,100 @@ class LessonPlanExpand extends Component
   render()
   {
     const lessonPlan = this.props.store.currentLessonPlan;
+    const { isDeleteModalOpen } = this.state;
     const isDisabled = this.props.store.isSaving || lessonPlan.name === "";
+    const isDeleteDisabled = this.props.store.currentLessonPlanID === "";
     return (
-      <form onSubmit={this.onSubmit}>
-        <div className="lesson_plan_expand">
+      <div>
+        <form onSubmit={this.onSubmit}>
+          <div className="lesson_plan_expand">
 
-          <label htmlFor="name">Class name</label>
-          <input 
-            name="name"
-            id="name"
-            value ={lessonPlan.name}
-            onChange={this.onChange}
-            placeholder="Class name"
-          />
+            <label htmlFor="name">Class name</label>
+            <input 
+              name="name"
+              id="name"
+              value ={lessonPlan.name}
+              onChange={this.onChange}
+              placeholder="Class name"
+            />
 
-          <label htmlFor="date">Date</label>
-          <DatePicker 
-            id="date"
-            selected={lessonPlan.date}
-            onChange={this.dateChange}
-            showTimeSelect
-            timeFormat="HH:mm"
-            timeIntervals={15}
-            dateFormat="MMMM d, yyyy h:mm aa"
-            timeCaption="time"
-          />
+            <label htmlFor="date">Date</label>
+            <DatePicker 
+              id="date"
+              selected={lessonPlan.date}
+              onChange={this.dateChange}
+              showTimeSelect
+              timeFormat="HH:mm"
+              timeIntervals={15}
+              dateFormat="MMMM d, yyyy h:mm aa"
+              timeCaption="time"
+            />
 
-          <label htmlFor="duration">Duration</label>
-          <input 
-            name="duration"
-            id="duration"
-            type="number"
-            value ={lessonPlan.duration}
-            onChange={this.onChange}
-            placeholder="lesson length"
-          /> mins.
-        </div>
+            <label htmlFor="duration">Duration</label>
+            <input 
+              name="duration"
+              id="duration"
+              type="number"
+              value ={lessonPlan.duration}
+              onChange={this.onChange}
+              placeholder="lesson length"
+            /> mins.
+          </div>
 
-        <div>
-          <label htmlFor="age">Age group</label>
-          <input 
-            name="age"
-            id="age"
-            value ={lessonPlan.age}
-            onChange={this.onChange}
-            placeholder="Age group"
-          />
+          <div>
+            <label htmlFor="age">Age group</label>
+            <input 
+              name="age"
+              id="age"
+              value ={lessonPlan.age}
+              onChange={this.onChange}
+              placeholder="Age group"
+            />
 
-          <label htmlFor="level">Skill level</label>
-          <input 
-            name="level"
-            id="level"
-            value ={lessonPlan.level}
-            onChange={this.onChange}
-            placeholder="Skill level"
-          />
-        </div>
+            <label htmlFor="level">Skill level</label>
+            <input 
+              name="level"
+              id="level"
+              value ={lessonPlan.level}
+              onChange={this.onChange}
+              placeholder="Skill level"
+            />
+          </div>
 
-        <div>
-          <label htmlFor="description">Description</label>
-          <textarea
-            name="description"
-            id="description"
-            value={lessonPlan.description}
-            onChange={this.onChange}
-            placeholder="Enter a description for your lesson plan"
-          />
-        </div>
+          <div>
+            <label htmlFor="description">Description</label>
+            <textarea
+              name="description"
+              id="description"
+              value={lessonPlan.description}
+              onChange={this.onChange}
+              placeholder="Enter a description for your lesson plan"
+            />
+          </div>
 
-        <ActivityList type="lessonPlanExpand" className="activity_list"/>
+          <ActivityList type="lessonPlanExpand" className="activity_list"/>
 
-        <button 
-          type="submit"
-          disabled={isDisabled}>
-          save
-        </button>
-      </form>
+          <button 
+            type="button"
+            disabled={isDeleteDisabled}
+            onClick={this.toggleDeleteModalOpen}
+          >
+            delete
+          </button>
+
+          <button 
+            type="submit"
+            disabled={isDisabled}>
+            save
+          </button>
+        </form>
+
+        <DeleteModal 
+          isOpen={isDeleteModalOpen}
+          toggleModal={this.toggleDeleteModalOpen}
+          onDelete={this.deleteEvent}
+        />
+      </div>
     );
 
   }
