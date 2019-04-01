aeee136da5bd26014238d62ab35b67e97928d9c5
Liam Frye-Mason <lfryemason>
2019-04-01 13:00:40 -0600

    Added styling to sign in page

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -6,87 +6,111 @@ import { SignUpLink } from '../SignUp';
 import { PasswordForgetLink } from '../PasswordForget';
 import * as ROUTES from '../../constants/routes';
 
+import logo_text from '../../res/Strateachy_logo_text.png';
+
+import './index.css';
+
 const SignInPage = () => (
-    <div>
-        <h1>SignIn</h1>
-        <SignInForm />
-    </div>
+  <div className="sign_in_page">
+
+    <img
+      className="sign_in_logo"
+      rel="Strateachy"
+      src={logo_text}
+      alt="Logo"
+    />
+
+    <SignInForm />
+  </div>
 )
 
 const INITIAL_STATE =
 {
-    email: '',
-    password: '',
-    error: null,
+  email: '',
+  password: '',
+  error: null,
 }
 
-class SignInFormBase extends Component 
-{
-    constructor(props)
-    {
-        super(props);
+class SignInFormBase extends Component {
+  constructor(props) {
+    super(props);
 
-        this.state = { ...INITIAL_STATE };
-    }
+    this.state = { ...INITIAL_STATE };
+  }
 
-    onSubmit = event =>
-    {
-        const { email, password } = this.state;
-        this.props.firebase
-            .doSignInWithEmailAndPassword(email, password)
-            .then(() => {
-                this.setState({ ...INITIAL_STATE})
-                this.props.history.push(ROUTES.HOME);
-            })
-            .catch(error => {
-                this.setState({ error });
-            });
-        event.preventDefault();
-    }
+  onSubmit = event => {
+    const { email, password } = this.state;
+    this.props.firebase
+      .doSignInWithEmailAndPassword(email, password)
+      .then(() => {
+        this.setState({ ...INITIAL_STATE })
+        this.props.history.push(ROUTES.HOME);
+      })
+      .catch(error => {
+        this.setState({ error });
+      });
+    event.preventDefault();
+  }
 
-    onChange = event =>
-    {
-        this.setState({ [event.target.name]: event.target.value });
-    };
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
-    render()
-    {
-        const { email, password, error } = this.state;
-    
-        const isInvalid = password === '' || email === '';
-    
-        return (
-            <form onSubmit={this.onSubmit}>
-                <input
-                    name="email"
-                    value={email}
-                    onChange={this.onChange}
-                    type="text"
-                    placeholder="Email Address"
-                />
-                <input
-                    name="password"
-                    value={password}
-                    onChange={this.onChange}
-                    type="password"
-                    placeholder="Password"
-                />
-                <PasswordForgetLink />
-                <SignUpLink />
-                <button disabled={isInvalid} type="submit">
-                    Sign In
-                </button>
+  render() {
+    const { email, password, error } = this.state;
 
-                {error && <p>{error.message}</p>}
-            </form>
-        );
-    }
+    const isInvalid = password === '' || email === '';
+
+    return (
+      <form 
+        onSubmit={this.onSubmit}
+        className="sign_in_form"
+      >
+
+        <label for="sign_in_email">Email</label>
+        <input
+          name="email"
+          value={email}
+          onChange={this.onChange}
+          type="text"
+          placeholder="Email Address"
+          className="sign_in_email sign_in_input"
+        />
+
+        <label for="sign_in_password">Password</label>
+        <input
+          name="password"
+          value={password}
+          onChange={this.onChange}
+          type="password"
+          placeholder="Password"
+          className="sign_in_password sign_in_input"
+        />
+
+        <PasswordForgetLink 
+          className="sign_in_forgot"
+        />
+        
+        <SignUpLink />
+        
+        <button 
+          disabled={isInvalid} 
+          type="submit"
+          className="sign_in_button"
+        >
+          Sign In
+        </button>
+
+        {error && <p>{error.message}</p>}
+      </form>
+    );
+  }
 }
 
 const SignInLink = () => (
-    <p>
-        Already have an account? <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-    </p>
+  <p>
+    Already have an account? <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+  </p>
 );
 
 
